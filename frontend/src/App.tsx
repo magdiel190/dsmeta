a299ba287d9642dea9540ae35888dc8cd4c626c5
Magdiel Wendel <magwendel190@gmail.com>
2022-11-15 18:19:23 -0300

    Outros componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import NotificationButton from "./components/notificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Ola Mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
