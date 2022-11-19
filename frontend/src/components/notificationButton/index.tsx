813ddcaa6aec1c3a8957a568afdf452367db1948
Magdiel Wendel <magwendel190@gmail.com>
2022-11-19 20:44:44 -0300

    Send notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,8 +1,21 @@
 import icon from "../../assets/img/notification-icon.svg";
+import axios from 'axios';
 import "./styles.css";
-function NotificationButton() {
+import { BASE_URL } from "../../utils/request";
+
+type Props = {
+    saleId: number;
+}
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(Response => {
+        console.log("Sucesso");
+    })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
